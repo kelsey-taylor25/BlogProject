20c435012aad75c96b4254abcac612142a6e29dd
ben-ayy <c2003386@ncl.ac.uk>
2023-09-28 12:34:54 +0100

    Finished practical 2

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+from blog.views import blog_blueprint
+app.register_blueprint(blog_blueprint)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from main.views import main_blueprint
+app.register_blueprint(main_blueprint)
+
+from users.views import users_blueprint
+app.register_blueprint(users_blueprint)
 
 
 if __name__ == '__main__':
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
+
+@users_blueprint.route('/register')
+def register():
+    return render_template('users/register.html')
+
+
+@users_blueprint.route('/login')
+def login():
+    return render_template('users/login.html')
+
+
+@users_blueprint.route('/account')
+def account():
+    return render_template('users/account.html')
